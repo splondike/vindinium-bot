85d744be21c783887afcc4ae950d3658dfcbe69f
Stefan Schneider-Kennedy <code@stefansk.name>
2017-04-14 15:51:18 +1000

    Existing rule tweaks and chase other bot rule

--- src/Bot.hs
+++ src/Bot.hs
@@ -5,7 +5,8 @@ module Bot
         )
     where
 
-import Data.Ord (comparing)
+import Data.String(fromString)
+import Data.Ord (comparing, Down)
 import Data.List (sortBy)
 import Data.Maybe (catMaybes, fromJust, isJust)
 
@@ -18,7 +19,6 @@ import qualified Data.Map.Strict as Map
 import qualified BotHelper as BH
 import qualified Vindinium as V
 
--- | Some quick global state
 type MineTurnovers = Map.Map V.Pos (LastOwner, Int)
 type LastOwner = Maybe V.HeroId
 
@@ -31,8 +31,9 @@ bot :: V.Bot BotState
 bot state = updateMineTurnovers state >> ST.get >>= \mineTurnovers -> pickRule [
                ("heal to full", tupleRule (notInHealLoopPosition && nextToTavern && notFullHealth && haveTavernMoney, goToNearestTavern)),
                ("snipe hero", trySnipeHero),
-               ("low health", tupleRule (lowHealth && haveTavernMoney, goToNearestTavern)),
+               ("low health", tupleRule (haveTavernMoney && lowHealth, goToNearestTavern)),
                ("i'm winning", tupleRule (earningRateHighest && wealthLargest, goToNearestTavern)),
+               ("attack high earning hero", chaseOtherHero),
                ("go to mine", tupleRule (True, goToBestMine mineTurnovers))
             ]
    where
@@ -42,6 +43,7 @@ bot state = updateMineTurnovers state >> ST.get >>= \mineTurnovers -> pickRule [
                                  [] -> ("no matching rules", V.Stay)
                                  (name, Just action):_ -> (name, action)
          lift $ V.logDebug name
+         lift . V.logDebug . fromString $ "threat level: " ++ (show threatLevel)
          return action
       tupleRule (True, action) = Just action
       tupleRule _ = Nothing
@@ -50,34 +52,61 @@ bot state = updateMineTurnovers state >> ST.get >>= \mineTurnovers -> pickRule [
                           [] -> Nothing
                           enemy:_ -> if imHealthy then snipeHero enemy else Nothing
          where
-            imHealthy = (>50) $ V.heroLife myHero
+            imHealthy = (>20) $ V.heroLife myHero
             snipeHero enemy = fmap head $ BH.movementsTo state (V.heroPos enemy)
-            isSnipable = (<40) . V.heroLife
-            closeHeroes = map fst $ filter ((==2) . length . snd) justPaths
-            justPaths = foldl extractSuccess [] heroTuples
-            extractSuccess xs (_, Nothing) = xs
-            extractSuccess xs (hero, Just path) = (hero, path):xs
-            heroTuples = zip otherHeroes $ map (BH.movementsTo state . V.heroPos) otherHeroes
+            isSnipable = (<=20) . V.heroLife
+            closeHeroes = map fst $ filter ((==2) . length . snd) heroPaths
 
-      notInHealLoopPosition = case filter isHero (BH.adjacentTiles board myPos) of
-                                   [] -> True
-                                   heroes -> not $ any touchingTavern heroes
+      chaseOtherHero = if imHealthy then chaseOtherHero' else Nothing
          where
-            isHero (_, V.HeroTile _) = True
-            isHero _ = False
-            touchingTavern (heroPos, _) = any (\(_, t) -> t == V.TavernTile) $ BH.adjacentTiles board heroPos
+            chaseOtherHero' = case filter goodTarget orderedHeroTuples of
+                                 [] -> Nothing
+                                 (enemy, path):_ -> Just $ head path
+
+            imHealthy = (>40) $ V.heroLife myHero
+            snipeHero enemy = fmap head $ BH.movementsTo state (V.heroPos enemy)
+            orderedHeroTuples = reverse $ sortBy (comparing attractiveness) heroPaths
+
+            goodTarget (hero, path) = V.heroMineCount hero >= 5 &&
+                                      length path <= 10 &&
+                                      (not . any (== V.TavernTile) $ adjacentTiles hero)
+            attractiveness (hero, path) = V.heroMineCount hero -
+                                          (fromIntegral $ length path `div` 2)
+            adjacentTiles hero = map snd $ BH.adjacentTiles board $ V.heroPos hero
+
+      notInHealLoopPosition = case filter isLoopHero adjacentTiles of
+                                   [] -> True
+                                   heroes -> False
+         where
+            isLoopHero (V.HeroTile hid) = any (==V.TavernTile) (tilesNextTo hid)
+            isLoopHero _ = False
+            tilesNextTo hid = map snd $ BH.adjacentTiles board $ V.heroPos $ heroById hid
       haveTavernMoney = 10 < V.heroGold myHero
       nextToTavern = any (== V.TavernTile) adjacentTiles 
       adjacentTiles = map snd $ BH.adjacentTiles board myPos
       notFullHealth = (90>) $ V.heroLife myHero
-      lowHealth = (50>) $ V.heroLife myHero
+      lowHealth = ((30 + 2*threatLevel)>) $ V.heroLife myHero
+      -- | 0-10 indicating how close other heroes are, 10 is they are closer
+      threatLevel = round $ min 10 (lenSum / 2)
+         where
+            -- 0-40, where 40 means very high threat
+            lenSum = fromIntegral $ sum $ map (invert . (min 11) . length . snd) heroPaths
+            invert len = 10 - (len - 1)
       earningRateHighest = playerBetterAt V.heroMineCount (\p c -> (p * 10) > (c * 13))
-      wealthLargest = playerBetterAt V.heroGold (>)
+      wealthLargest = playerBetterAt V.heroGold (\p c -> (p*10) > (c*13))
       playerBetterAt prop comparator = comparator (prop myHero) (prop competition)
          where
             competition = bestOtherHeroBy (comparing prop)
       bestOtherHeroBy comparator = head $ reverse $ sortBy comparator otherHeroes
+      -- | Paths to all other heroes I can reach
+      heroPaths :: [TargetAndPath V.Hero]
+      heroPaths = targetsWithPaths state V.heroPos otherHeroes
       otherHeroes = filter (/= myHero) $ V.gameHeroes $ V.stateGame state
+      heroById :: V.HeroId -> V.Hero
+      heroById hid = head $ filter match $ V.gameHeroes $ V.stateGame state
+         where
+            match hero = V.heroId hero == hid
+
 
       goToNearestTavern = goToClosest state (== V.TavernTile)
       goToBestMine turnoverMap = case orderedPairs of
@@ -85,7 +114,7 @@ bot state = updateMineTurnovers state >> ST.get >>= \mineTurnovers -> pickRule [
                                       (_, path):_ -> head path
          where
             orderedPairs = sortBy comparator pairs
-            comparator = comparing (\(c, p) -> c + (length p) * 5)
+            comparator = comparing (\(c, p) -> c + (length p) * 2)
             pairs = map (\(a, Just b) -> (a, b)) $ filter (isJust . snd) $ zip turnovers paths
             turnovers = map (extract) allMatches
             extract pos = snd $ Map.findWithDefault (Nothing, 0) pos turnoverMap
@@ -99,6 +128,14 @@ bot state = updateMineTurnovers state >> ST.get >>= \mineTurnovers -> pickRule [
       board = V.gameBoard $ V.stateGame state
       myPos = V.heroPos myHero
       myHero = V.stateHero state
+
+type TargetAndPath a = (a, [V.Dir])
+targetsWithPaths :: V.State -> (a -> V.Pos) -> [a] -> [TargetAndPath a]
+targetsWithPaths state posExtractor targets = foldl makeTuple [] targets
+   where
+      makeTuple xs a = case BH.movementsTo state $ posExtractor a of
+                            Nothing -> xs
+                            Just path -> (a, path):xs
 
 updateMineTurnovers :: V.State -> ST.StateT BotState V.Vindinium ()
 updateMineTurnovers state = ST.modify update
